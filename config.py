2597258971a778e27b7322bea9c088f8414325fd
advait <advait001@e.ntu.edu.sg>
2023-10-23 22:06:53 +0800

    Fix folder structure

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    MONGO_URI = "mongodb://localhost:27017/flask_mongo_db" 
+    MONGO_URI = "mongodb+srv://default:SCSX@a-train.pdzvpay.mongodb.net/?retryWrites=true&w=majority" 
     JWT_SECRET_KEY = 'your-secret-key-here' # generate a good secret key
