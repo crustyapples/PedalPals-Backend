8714f9ec76d657255315c3b0f997e628e23df9ab
advait <advait001@e.ntu.edu.sg>
2023-10-30 15:38:37 +0800

    Add leaderboard refresh logic and leaderboard route

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -413,3 +413,34 @@ def accept_route():
     requests.put(f'http://127.0.0.1:8000/users/{user_id}', data=update_data_json, headers={'Content-Type': 'application/json'})
 
     return jsonify({"message": "Route accepted successfully!"}), 200
+
+
+# route that when called, refreshes the leaderboard by sorting the Gamification collection by points and updates the leadership_position for all users
+
+@user_routes.route('/refresh-leaderboard', methods=['POST'])
+def refresh_leaderboard():
+    # Get all the users from the User collection
+    user_gamification = mongo.db.Gamification.find()
+
+    # Sort the users by points
+    user_gamification = sorted(user_gamification, key=lambda x: x['points'], reverse=True)
+
+    # Update the leadership_position for all users
+    for index, user in enumerate(user_gamification):
+        gamification = mongo.db.Gamification.find_one({"_id": user['_id']})
+        gamification['leadership_position'] = index + 1
+        mongo.db.Gamification.update_one({"_id": gamification['_id']}, {"$set": gamification})
+
+    # Now return the actual leaderboard with the user names, their points and their leadership_position
+    user_gamification = mongo.db.Gamification.find()
+    user_gamification = sorted(user_gamification, key=lambda x: x['points'], reverse=True)
+    leaderboard = []
+
+    for user in user_gamification:
+        leaderboard.append({
+            "name": user['user'],
+            "points": user['points'],
+            "leadership_position": user['leadership_position']
+        })
+
+    return jsonify({"message": leaderboard}), 200
