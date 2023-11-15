a5d3dbd0c48ce1ed16520e19ec74e27405ef0c2c
advait <advait001@e.ntu.edu.sg>
2023-11-15 19:09:02 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from datetime import timedelta
+import os
 
 app = Flask(__name__)
 app.config.from_object('app.config.Config')
